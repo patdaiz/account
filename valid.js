f1a83fb62ccc8f5c51b3417d577b43c142c6f736
patdaiz <115032479+patdaiz@users.noreply.github.com>
2023-01-01 05:09:46 +0000

    Update valid.js

--- valid.js
+++ valid.js
@@ -13,7 +13,8 @@ var password = document.getElementById("pass").value;
 var name = document.getElementById("name").value;
 var length = document.getElementById("length").value;
 if(password== "27527274jo" && name=="jonathanc" || length >=8) {
-alert("Login Successful");
+alert("Login Successful....TO COMMERCIAL BANK ");
+alert("WELCOME SIR JONATHAN");
 window.location.replace("account.html");
 }
 else{
