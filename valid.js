a12a16b002cd04d4aab90b19b493ec1d48dd9e8c
patdaiz <115032479+patdaiz@users.noreply.github.com>
2023-01-02 02:10:31 +0000

    new code

---
+++ valid.js
@@ -0,0 +1,24 @@
+function myfunction(){
+var x =document.getElementById("pass");
+if(x.type== "password"){
+   x.type = "text";
+}
+  else{
+x.type = "password";
+}
+}
+
+function validate(){
+var password = document.getElementById("pass").value;
+var name = document.getElementById("name").value;
+var length = document.getElementById("length").value;
+if(password== "27527274jo" && name=="jonathanc" || length >=8) {
+alert("Login Successful");
+alert("welcome jonathan");
+window.location.replace("account.html");
+}
+else{
+ alert("Login Failed");
+return;
+}
+}
