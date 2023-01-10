ff7d873bccf229de6bd9a20f37ff4248edbc62e8
patdaiz <115032479+patdaiz@users.noreply.github.com>
2023-01-10 17:15:34 +0000

    update js

--- valid.js
+++ valid.js
@@ -13,7 +13,7 @@ var password = document.getElementById("pass").value;
 var name = document.getElementById("name").value;
 var length = document.getElementById("length").value;
 if(password== "27527274jo" && name=="jonathanc" || length >=8) {
-alert("Login Successful.....WELCOME TO YOUR ACCOUNT");
+alert("Login Successful");
 alert("welcome jonathan");
 window.location.replace("account.html");
 }
@@ -22,3 +22,16 @@ else{
 return;
 }
 }
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        2500
+    )
+});
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+}
+)
